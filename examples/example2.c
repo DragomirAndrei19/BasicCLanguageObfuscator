b6cfcee7aa7484996f93493f88bcb7fe036214fb
Dragomir Andrei <56070218+DragomirAndrei19@users.noreply.github.com>
2022-02-17 20:55:01 +0200

    Create example2.c

---
+++ examples/example2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int largest(int arr[], int n)
+{
+    int i;
+    // Initialize maximum element
+    int max = arr[0];
+
+    // Traverse array elements from second and
+    // compare every element with current max  
+    for (i = 1; i < n; i++)
+        if (arr[i] > max)
+            max = arr[i];
+
+    return max;
+}
+
+int main()
+{
+    int arr[] = {10, 324, 45, 90, 9808};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    printf("Largest in given array is %d", largest(arr, n));
+    return 0;
+}
